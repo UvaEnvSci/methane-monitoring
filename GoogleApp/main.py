481861eba7a80a6bba881eeb6fa2c6722d771e2a
karshish <mv7b@virginia.edu>
2014-03-12 21:52:31 -0400

    JSON
    * added JSON to send data as an object to the website.
    * added new handler, '/delete', to delete all the entrys in the
    datastore.

--- GoogleApp/main.py
+++ GoogleApp/main.py
@@ -1,11 +1,14 @@
 """`main` is the top level module for your Flask application."""
 
 # Import the Flask Framework
-from flask import Flask, request, render_template
+import calendar, datetime, json
+from flask import Flask, request, render_template, Response
 from google.appengine.ext import ndb
+# from models import Entry
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+
 
 class Data(ndb.Model):
    datastringtime = ndb.DateTimeProperty(auto_now_add=True)
@@ -16,15 +19,21 @@ class Data(ndb.Model):
    pressure = ndb.FloatProperty()
    battery = ndb.FloatProperty()
    
+
+@app.route('/delete')
+def delete():
+   entries = Data.query().fetch(keys_only=True)
+   ndb.delete_multi(entries)
+   # This could bulk delete 1000 entities a time
+   return 'entries deleted'
+      
    
 @app.route('/')   
-   
 def home():
    query = Data.query().order(Data.datastringtime)
    return render_template('home.html',results=query)
    
 @app.route('/log')
-
 def log():
    datastringID = request.args.get('ID','NA')
    datastringtemp = request.args.get('T','9999')
@@ -36,7 +45,28 @@ def log():
    data.put()
    return '0'
 
+def handler(obj):
+#   if hasattr(obj, 'isoformat'):
+    if hasattr(obj, 'utctimetuple'):
+#      return obj.isoformat()
+       return calendar.timegm(obj.utctimetuple()) 
+    else:
+       raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
+   
+@app.route('/api')
+def api():
 
+   prevrequestdatetime = request.args.get('time',None)
+   if prevrequestdatetime is None:
+      query = Data.query().order(Data.datastringtime)
+   else:
+      dt = datetime.datetime.fromtimestamp(float(prevrequestdatetime))
+      query = Data.query(Data.datastringtime > dt).order(Data.datastringtime)
+   js = json.dumps([p.to_dict() for p in query.fetch()], default=handler)
+   resp = Response(js, status=200, mimetype='application/json')
+   return resp
+
+   
 @app.errorhandler(404)
 def page_not_found(e):
    """Return a custom 404 error."""
